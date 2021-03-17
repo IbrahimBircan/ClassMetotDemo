9e8c5bca2cf053eabb518f583b6f349f5f302bef
HIbrahimBircan@outlook.com <HIbrahimBircan@outlook.com>
2021-03-17 14:51:00 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Eklendi !"+ musteri.TC + " " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Silindi !" + musteri.TC + " " + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id =1;
+            musteri1.TC = "12345678901"; 
+            musteri1.Ad = "Ahmet";
+            musteri1.Soyad = "Deneme";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.TC = "12345678902";
+            musteri2.Ad = "Mehmet";
+            musteri2.Soyad = "Dağ";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.TC = "12345678903";
+            musteri3.Ad = "Ömer";
+            musteri3.Soyad = "Kaya";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 4;
+            musteri4.TC = "12345678904";
+            musteri4.Ad = "Hasan";
+            musteri4.Soyad = "Demir";
+
+            Musteri[] musteriler = new Musteri[] { musteri1,musteri2,musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+           
+
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.TC + " "+musteri.Ad +" "+musteri.Soyad);
+
+            }
+
+            musteriManager.Ekle(musteri4);
+            musteriManager.Sil(musteri2);
+        }
+    }
+}
